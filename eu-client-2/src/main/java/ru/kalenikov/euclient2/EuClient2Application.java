bf092b44a35843c74013b5d40e67b8a6b3c55f3b
Калеников Сергей Иванович <s.kalenikov@sigma-it.ru>
2023-01-13 12:32:21 +0300

    add api-gateway and client-2

---
+++ eu-client-2/src/main/java/ru/kalenikov/euclient2/EuClient2Application.java
@@ -0,0 +1,13 @@
+package ru.kalenikov.euclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuClient2Application.class, args);
+    }
+
+}
