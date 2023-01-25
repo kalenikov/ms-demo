9521ba692557734132ba55d449ca63c1f2c17d38
Калеников Сергей Иванович <s.kalenikov@sigma-it.ru>
2023-01-25 16:58:31 +0300

    eu1 + eu2 connect to config server

--- eu-client/src/main/java/ru/kalenikov/euclient/EuClient.java
+++ eu-client/src/main/java/ru/kalenikov/euclient/EuClient.java
@@ -1,13 +1,33 @@
 package ru.kalenikov.euclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class EuClient {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    @Value("${instance.title}")
+    private String title;
+
     public static void main(String[] args) {
         SpringApplication.run(EuClient.class, args);
     }
+
+    @GetMapping("/")
+    public String index() {
+        return Map.of("instance.title", title,
+                "eureka.instance.instance-id", id).toString();
+    }
+
 }
