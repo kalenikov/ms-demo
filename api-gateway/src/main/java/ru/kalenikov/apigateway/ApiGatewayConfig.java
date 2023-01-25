ea1e91891808e4212ea1247c9c5f2ecf27655f37
Калеников Сергей Иванович <s.kalenikov@sigma-it.ru>
2023-01-25 12:04:38 +0300

    ApiGateway ref

--- api-gateway/src/main/java/ru/kalenikov/apigateway/ApiGatewayConfig.java
+++ api-gateway/src/main/java/ru/kalenikov/apigateway/ApiGatewayConfig.java
@@ -2,13 +2,11 @@ package ru.kalenikov.apigateway;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
-@Configuration
-public class ProxyConfig {
-    @Bean
+//@Configuration
+public class ApiGatewayConfig {
+//    @Bean
     RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes().route(
                 "test_route", route -> route.path("/test/**")
